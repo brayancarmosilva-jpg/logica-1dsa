89f9b961c4f2d8a9643543b9a5b5d2e34f8e1488
brayancarmosilva-jpg <brayan.carmo.silva@escola.pr.gov.br>
2025-10-20 12:00:46 -0300

    Create laco-for.py

---
+++ laco-for.py
@@ -0,0 +1,7 @@
+# Variáveis
+frase = "Acredite em você e tudo será possível!"
+quantas_vezes = 5  # Número de vezes que a frase será repetida
+
+# Laço de repetição
+for i in range(quantas_vezes):
+    print(frase)
